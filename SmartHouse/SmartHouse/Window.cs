896754ea92407309ae0ec3680fc9145e22943580
osheeat <80705966+osheeat@users.noreply.github.com>
2023-11-10 09:54:32 +0500

    Add files via upload

---
+++ SmartHouse/SmartHouse/Window.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHouse
+{
+    public class Window
+    {
+        public Point First;
+        public Point Second;
+        public bool isOpened;
+
+        public Window(Point first, Point Second) 
+        {
+            this.First = first;
+            this.Second = Second;
+            this.isOpened = false;
+        }
+
+        public void Redraw(Point newWind)
+        {
+            isOpened = !isOpened;
+            this.Second.X = newWind.X;
+            this.Second.Y = newWind.Y;
+        }
+    }
+}
