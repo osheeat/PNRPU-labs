54f7975b8e1f1d6e4627c8173b25edc9c3de85c7
osheeat <80705966+osheeat@users.noreply.github.com>
2023-11-10 10:24:48 +0500

    Add files via upload

---
+++ 1stYear/CorrectNumber/CorrectNumber/CorrectNumber.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int getCorrectNumber(int num)
+{
+	while (!(cin >> num) || (cin.peek() != '\n') || num < 0)
+	{
+		cin.clear();
+		while (cin.get() != '\n');
+		cout << "Введите корректное число: ";
+	}
+	return num;
+}
